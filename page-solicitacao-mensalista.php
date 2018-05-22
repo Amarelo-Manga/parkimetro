577b2b9a0163af8db561fdf95e55d55229f04f10
Elvis B. Martins <elvisbmartins@gmail.com>
2018-05-22 17:46:55 -0300

    estrutura page solicitacao

--- page-solicitacao-mensalista.php
+++ page-solicitacao-mensalista.php
@@ -15,16 +15,20 @@ get_header(); ?>
 <div id="solicitacao">
 	<section id="breadcrumb">
 		<div class="container">
-			<a href="#">Escolha uma unidade</a>
-			<a href="#">Escolha valor e horário</a>
-			<a href="#">Preencher dados</a>
-			<a href="#">Solicitação de vaga concluída</a>
+			<a href="#" class="active">Escolha uma unidade <i class="fa fa-caret-right" aria-hidden="true"></i><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			<a href="#" class="escolha-horario">Escolha valor e horário <i class="fa fa-caret-right" aria-hidden="true"></i><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			<a href="#" class="preencher-dados">Preencher dados <i class="fa fa-caret-right" aria-hidden="true"></i><i class="fa fa-angle-right" aria-hidden="true"></i></a>
+			<a href="#" class="concluir">Solicitação de vaga concluída <i class="fa fa-caret-right" aria-hidden="true"></i><i class="fa fa-angle-right" aria-hidden="true"></i></a>
 		</div>
 	</section>
-	<section id="etapa1" class="container">
+	<section id="etapa1" class="container etapa">
 		<div id="filtros"  class="row">
-			<div class="col-lg-6 col-md-6 col-sm-12 mb-3">
-				<select id="bootstrapSelectEst" class="selectpicker" data-show-subtext="true" data-live-search="true">	
+			<div class="col-lg-6 col-md-6 col-sm-12">
+				<h3>
+					Escolha a unidade desejada e verifique<br />a disponibilidade de vagas e horários.
+				</h3>
+				<select id="bootstrapSelectEst" class="selectpicker" data-show-subtext="true" data-live-search="true">
+				<option value="">Escolha o Estacionamento</option>
 				<?php 
 		            $args = array(
 		                'posts_per_page' => -1,
@@ -48,43 +52,63 @@ get_header(); ?>
 							$est .= " ".$e->name;
 						}
 		 
-		        		echo '<option value="'.$cod.'" data-subtext="'.$est.' - '.$mun .'">'.$cod.' - '.$end.'</option>';
+		        		echo '<option value="'.$post->ID.'" data-subtext=" - '.$est.'">'.$cod.' - '.$end.'</option>';
 
 						$count++;
 		            endwhile;
 		            wp_reset_query();
 				?>
 				</select>
+				<a href="#" class="btn-passo1 btn-passo">Próximo passso</a>
 			</div>
-			<div class="col-lg-6 col-md-6 col-sm-12 mb-3">
-				<div class="">
-					<h3>vagas para mensalistas</h3>
+			<div class="col-lg-6 col-md-6 col-sm-12">
+				<div class="infos">
+					<h3>Vagas para mensalistas</h3>
 					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+				</div>
+				<br />
+				<div class="infos">
+					<h3>Login</h3>
+					<div class="form-group">
+						<br />
+						<div class="col-md-5">
+							<label class="control-label" for="telefone">Usuário</label> 
+							<input id="usuario" name="usuario" type="text" placeholder="Fone" class="form-control input-md" required="">
+						</div>
+						<div class="col-md-5">
+							<label class="control-label" for="telefone">Senha</label> 
+							<input id="senha" name="senha" type="password" placeholder="senha" class="form-control input-md" required="">
+						</div>
+						<br /><br /><br /><br /><br />
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
-	<hr />
-	<section id="etapa2"> 
+
+	<section id="etapa2" class="etapa"> 
 		<div class="container">
-			<h3>COD - UNIDADE XPTO</h3>
-			<p>Selecione abaixo a vaga desejada nesta unidade e escolha a quantidade de vagas necessárias</p>
-			<div id="lista">
-				<ul>
-					<li>QTD</li>
-					<li>Descrição</li>
-					<li>Horario</li>
-					<li>Valor</li>
-				</ul>
-			</div>
 			<div id="vagas">
 				<?php
 					// check if the repeater field has rows of data
 					while ($loop_rede->have_posts()) : $loop_rede->the_post();
 						$qtd = get_post_meta( $post->ID, 'quantidade-vagas', true );
+						$end = get_post_meta( $post->ID, 'endereco', true );
+		            	$cod = get_post_meta( $post->ID, 'cod_unidade', true );
+
+				?>
+					<div class="estacionamentos estacionamento-<?php echo $post->ID; ?>" >
+						<h3><?php echo $cod .' - ' . $end; ?></h3>
+						<p>Selecione abaixo a vaga desejada nesta unidade e escolha a quantidade de vagas necessárias</p>
+					<?php 
 						if( !empty( $qtd ) ){
-							echo "Quantida de vagas do Estacionamento: " . $qtd;	
-						}
+							echo "<h4> Quantidade de vagas do Estacionamento: " . $qtd ."</h4>";	
+					?>
+						<ul class="lista">
+							<li>Quantidade</li><li>Descrição</li><li>Horário</li><li>Valor</li>
+						</ul>
+					<?php
+						}//EndIf Empty QTD
 						if( have_rows('tabelas-precos', $post->ID ) ):
 						    while ( have_rows('tabelas-precos', $post->ID ) ) : the_row();
 						    	$descricao 		= get_sub_field('descricao');
@@ -98,13 +122,17 @@ get_header(); ?>
 						    	echo "</ul>";
 						     endwhile;
 						endif; //endif vagas
+					?>
+					</div>
+				<?php 
 					 endwhile;//endWhile $loop_rede
 				?>
+				<a href="#" class="btn-passo2 btn-passo">Próximo passso</a>
 			</div>
 		</div>
 	</section>
-	<hr />
-	<section id="etapa3"> 
+
+	<section id="etapa3" class="etapa"> 
 		<div class="container">
 			<form class="form-horizontal">
 				<fieldset>
@@ -112,8 +140,8 @@ get_header(); ?>
 					<legend>Informações de Solicitação</legend>
 					<!-- Multiple Checkboxes -->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="infos">Informações</label>
-						<div class="col-md-4">
+						<label class="col-md-12 control-label" for="infos">Informações</label>
+						<div class="col-md-12">
 							<div class="checkbox">
 								<label for="infos-0">
 								<input type="checkbox" name="infos" id="infos-0" value="diurno">
@@ -148,119 +176,102 @@ get_header(); ?>
 					</div>
 					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="nome">Nome Usuário</label>  
-						<div class="col-md-4">
+						<div class="col-md-8">
+							<label class="control-label" for="nome">Nome Usuário</label> 
 							<input id="nome" name="nome" type="text" placeholder="Nome Usuário" class="form-control input-md" required="">
 						</div>
-					</div>
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="cpf">CPF</label>  
 						<div class="col-md-4">
+							<label class="control-label" for="cpf">CPF</label>  
 							<input id="cpf" name="cpf" type="text" placeholder="CPF" class="form-control input-md" required="">
 						</div>
 					</div>
 					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="telefone">Fone</label>  
-						<div class="col-md-4">
+					<div class="form-group">						
+						<div class="col-md-5">
+							<label class="control-label" for="telefone">Fone</label> 
 							<input id="telefone" name="telefone" type="text" placeholder="Fone" class="form-control input-md" required="">
 						</div>
-					</div>
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="celuar">Celular</label>  
-						<div class="col-md-4">
+						<div class="col-md-5">
+							<label class="control-label" for="celuar">Celular</label> 
 							<input id="celuar" name="celuar" type="text" placeholder="Celular" class="form-control input-md" required="">
 						</div>
 					</div>
 					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="email">Email</label>  
-						<div class="col-md-4">
+						<div class="col-md-12">
+							<label class="control-label" for="email">Email</label>  
 							<input id="email" name="email" type="text" placeholder="Email" class="form-control input-md" required="">
 						</div>
 					</div>
 					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="endereco">Endereço</label>  
-						<div class="col-md-4">
+						<div class="col-md-12">
+							<label class="control-label" for="endereco">Endereço</label>  
 							<input id="endereco" name="endereco" type="text" placeholder="Endereço" class="form-control input-md" required="">
 							<span class="help-block">RUA/ AVENIDA/ TRAVESSSA - NÚMERO/BLOCO/APARTAMENTO</span>  
 						</div>
 					</div>
-					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="bairro">Bairro</label>  
+						
 						<div class="col-md-4">
-							<input id="bairro" name="bairro" type="text" placeholder="Bairro" class="form-control input-md">
-						</div>
-					</div>
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="cep">CEP</label>  
-						<div class="col-md-4">
+							<label class="control-label" for="cep">CEP</label>  
 							<input id="cep" name="cep" type="text" placeholder="CEP" class="form-control input-md" required="">
 						</div>
-					</div>
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="cidade">Cidade - UF</label>  
 						<div class="col-md-4">
+							<label class="control-label" for="bairro">Bairro</label> 
+							<input id="bairro" name="bairro" type="text" placeholder="Bairro" class="form-control input-md">
+						</div>
+						<div class="col-md-4">
+							<label class="control-label" for="cidade">Cidade - UF</label> 
 							<input id="cidade" name="cidade" type="text" placeholder="Cidade - UF" class="form-control input-md" required="">
 						</div>
 					</div>
 					<!-- Text input-->
+		
+					<legend>Informações de Solicitação - Empresa</legend>
+					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="empresa">Empresa</label>  
-						<div class="col-md-4">
+						<div class="col-md-12">
+							<label class="control-label" for="empresa">Empresa</label> 
 							<input id="empresa" name="empresa" type="text" placeholder="Empresa" class="form-control input-md">
 						</div>
 					</div>
 					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="fone-empresa">Fone Empresa</label>  
-						<div class="col-md-4">
+						<div class="col-md-5">
+							<label class="control-label" for="fone-empresa">Fone Empresa</label>  
 							<input id="fone-empresa" name="fone-empresa" type="text" placeholder="Fone Empresa" class="form-control input-md">
 						</div>
-					</div>
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="ramal-empresa">Ramal Empresa</label>  
 						<div class="col-md-4">
+							<label class="control-label" for="ramal-empresa">Ramal Empresa</label> 
 							<input id="ramal-empresa" name="ramal-empresa" type="text" placeholder="Ramal Empresa" class="form-control input-md">
 						</div>
 					</div>
 					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="endereco-empresa">Endereço Empresa</label>  
-						<div class="col-md-4">
+						<div class="col-md-12">
+							<label class="control-label" for="endereco-empresa">Endereço Empresa</label>
 							<input id="endereco-empresa" name="endereco-empresa" type="text" placeholder="Endereço Empresa" class="form-control input-md">
 						</div>
+						<div class="col-md-12">
+							<label class="control-label" for="complemento-empresa">Complemento Empresa</label> 
+							<input id="complemento-empresa" name="complemento-empresa" type="text" placeholder="Complemento Empresa" class="form-control input-md">
+						</div>
 					</div>
 					<!-- Text input-->
 					<div class="form-group">
-						<label class="col-md-4 control-label" for="cep-empresa">CEP Empresa</label>  
 						<div class="col-md-4">
+							<label class="control-label" for="cep-empresa">CEP Empresa</label>  
 							<input id="cep-empresa" name="cep-empresa" type="text" placeholder="CEP Empresa" class="form-control input-md">
 						</div>
-					</div>
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="cidade-empresa">Cidade Empresa</label>  
 						<div class="col-md-4">
+							<label class="control-label" for="cidade-empresa">Cidade Empresa</label>
 							<input id="cidade-empresa" name="cidade-empresa" type="text" placeholder="Cidade Empresa" class="form-control input-md">
-						</div>
-					</div>
-					<!-- Text input-->
-					<div class="form-group">
-						<label class="col-md-4 control-label" for="complemento-empresa">Complemento Empresa</label>  
-						<div class="col-md-4">
-							<input id="complemento-empresa" name="complemento-empresa" type="text" placeholder="Complemento Empresa" class="form-control input-md">
 						</div>
 					</div>
 				</fieldset>
 			</form>
+			<legend>Informações de Solicitação - Veiculo</legend>
 			<form class="form-horizontal" id="veiculos">
 				<div id="myRepeatingFields">
 					<div class="entry input-group col-xs-3">
@@ -285,13 +296,15 @@ get_header(); ?>
 						</span>
 					</div>
 				</div>	
-			</form>			
+			</form>	
+			<a href="#" class="btn-passo3 btn-passo">Próximo passso</a>		
 		</div>
 	</section>
-	<hr />
-	<section id="solicitacao"> 
+
+	<section id="etapa4" class="etapa"> 
 		<div class="container">
 			<h3>Solicitação enviada com sucesso!</h3>
+			<p>Nossa equipe irá avaliar seu pedido e entrará em contato.</p>
 		</div>
 	</section>
 </div>
