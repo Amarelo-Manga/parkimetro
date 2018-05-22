591d8efe907d2dbfb7b78e68e1d8ca12409a1455
Elvis B. Martins <elvisbmartins@gmail.com>
2018-05-22 17:46:35 -0300

    scripts pagina solicitacao frontend

--- assets/js/global.js
+++ assets/js/global.js
@@ -312,9 +312,35 @@
 
 	//Pagina Solicitacao de Mensalista, Escolha do Estacionamento Bootstrap Select  
 	$('#bootstrapSelectEst').on('change', function(e){
-	  console.log(this.value);
+	  	console.log(this.value);
+	  	var estacionamento = '.estacionamento-' + this.value;
+	  	$('.estacionamentos').hide(100);
+	  	$(estacionamento).show(100);
+
 	});
 
+	$('.btn-passo1').on('click', function(){
+		$('#etapa1').hide(100);
+		$('#etapa2').show(100);
+		$('a.active').addClass('conclused');
+		$('a.conclused').removeClass('active');
+		$('.escolha-horario').addClass('active');
+	});
+	$('.btn-passo2').on('click', function(){
+		$('#etapa2').hide(100);
+		$('#etapa3').show(100);
+		$('a.active').addClass('conclused');
+		$('a.conclused').removeClass('active');
+		$('.preencher-dados').addClass('active');
+	});
+
+	$('.btn-passo3').on('click', function(){
+		$('#etapa3').hide(100);
+		$('#etapa4').show(100);
+		$('a.active').addClass('conclused');
+		$('a.conclused').removeClass('active');
+		$('.concluir').addClass('active');
+	});
 
 	// Repeat Fields Descrição dos Veiculos
 	$(document).on('click', '.btn-add', function(e)
