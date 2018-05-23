6f8d3632bd6c5835e3b48c68528669631c612270
Elvis B. Martins <elvisbmartins@gmail.com>
2018-05-23 17:57:56 -0300

    remove scripts solicitacao do global js

--- assets/js/global.js
+++ assets/js/global.js
@@ -310,56 +310,5 @@
 	    .replace(/ç/g, 'c');
 	}
 
-	//Pagina Solicitacao de Mensalista, Escolha do Estacionamento Bootstrap Select  
-	$('#bootstrapSelectEst').on('change', function(e){
-	  	console.log(this.value);
-	  	var estacionamento = '.estacionamento-' + this.value;
-	  	$('.estacionamentos').hide(100);
-	  	$(estacionamento).show(100);
-
-	});
-
-	$('.btn-passo1').on('click', function(){
-		$('#etapa1').hide(100);
-		$('#etapa2').show(100);
-		$('a.active').addClass('conclused');
-		$('a.conclused').removeClass('active');
-		$('.escolha-horario').addClass('active');
-	});
-	$('.btn-passo2').on('click', function(){
-		$('#etapa2').hide(100);
-		$('#etapa3').show(100);
-		$('a.active').addClass('conclused');
-		$('a.conclused').removeClass('active');
-		$('.preencher-dados').addClass('active');
-	});
-
-	$('.btn-passo3').on('click', function(){
-		$('#etapa3').hide(100);
-		$('#etapa4').show(100);
-		$('a.active').addClass('conclused');
-		$('a.conclused').removeClass('active');
-		$('.concluir').addClass('active');
-	});
-
-	// Repeat Fields Descrição dos Veiculos
-	$(document).on('click', '.btn-add', function(e)
-	{
-		e.preventDefault();
-		var controlForm = $('#myRepeatingFields:first'),
-			currentEntry = $(this).parents('.entry:first'),
-			newEntry = $(currentEntry.clone()).appendTo(controlForm);
-		newEntry.find('input').val('');
-		controlForm.find('.entry:not(:last) .btn-add')
-			.removeClass('btn-add').addClass('btn-remove')
-			.removeClass('btn-success').addClass('btn-danger')
-			.html('<span class="glyphicon glyphicon-minus"></span>');
-	}).on('click', '.btn-remove', function(e)
-	{
-		e.preventDefault();
-		$(this).parents('.entry:first').remove();
-		return false;
-	});
-
 
 })( jQuery );
